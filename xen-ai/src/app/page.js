19de36dadd6b0f1b64fe3326e6d976cabdcbe35f
shubhFreelance <shubham58ch@gmail.com>
2024-03-10 20:57:32 +0530

    hero design fixed

--- xen-ai/src/app/page.js
+++ xen-ai/src/app/page.js
@@ -1,6 +1,7 @@
 'use client'
 
 import Image from "next/image";
+import Styles from "./page.module.css"
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import { useSpring } from '@react-spring/core';
@@ -9,16 +10,16 @@ import Overlay from './Overlay';
 import Scene from './Scene';
 
 export default function Home() {
-  const [{ background, fill }, set] = useSpring({ background: '#f0f0f0', fill: '#202020' }, [])
+  const [{ background, fill }, set] = useSpring({ background: 'rgb(18, 17, 17)', fill: '#202020' }, [])
   return (
     <>
-    <div className="container">
-    <Canvas className="canvas" dpr={[1, 2]}>
+    <a.main style={{ background }}>
+      <Canvas className="canvas" dpr={[1, 2]}>
         <Scene setBg={set} />
         <OrbitControls enablePan={false} enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
       </Canvas>
       <Overlay fill={fill} />
-    </div>
+    </a.main>
     </>
   );
 }
